4c39183f4e5d5c5a58d7896a0b446a9b7d026078
alicema1202 <64507532+alicema1202@users.noreply.github.com>
2022-04-29 14:13:11 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,9 +17,11 @@ public class MarkdownParse {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;            
+            int closeParen = markdown.indexOf(")", openParen);                
+            if (closeBracket + 1 == openParen) {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+            currentIndex = closeParen + 1;         
         }
         
         return toReturn;
